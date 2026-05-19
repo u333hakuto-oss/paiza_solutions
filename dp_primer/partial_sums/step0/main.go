759d40ec5bd18febe162fdf890197380411a5bfd
sumire <u333hakuto@gmail.com>
2026-05-19 18:51:50 +0900

    init

---
+++ dp_primer/partial_sums/step0/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	buf := bufio.NewReader(os.Stdin)
+	var n, x int
+	fmt.Fscan(buf, &n, &x)
+	weights := make([]int, n+1)
+	for i := 1; i <= n; i++ {
+		fmt.Fscan(buf, &weights[i])
+	}
+	dp := make([]bool, x+1)
+	dp[0] = true
+	for i := 1; i <= n; i++ {
+		for j := x; j >= weights[i]; j-- {
+			if dp[j-weights[i]] {
+				dp[j] = true
+			}
+		}
+	}
+	if dp[x] {
+		fmt.Println("yes")
+	} else {
+		fmt.Println("no")
+	}
+}

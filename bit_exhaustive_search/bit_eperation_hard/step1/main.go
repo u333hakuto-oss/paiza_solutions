12255931c5cafacac6d77a2cc6ccf19b9adb01c3
sumire <u333hakuto@gmail.com>
2026-08-05 19:19:27 +0900

    perf: optimize bitwise range queries with prefix sums and prefix XOR

--- bit_exhaustive_search/bit_eperation_hard/step1/main.go
+++ bit_exhaustive_search/bit_eperation_hard/step1/main.go
@@ -7,38 +7,48 @@ import (
 )
 
 func main() {
+	const Bits = 30
 	buf := bufio.NewReader(os.Stdin)
 	var n, q int
 	fmt.Fscan(buf, &n)
-	p := make([]int, n)
-	for i := 0; i < n; i++ {
+	p := make([]int, n+1)
+	for i := 1; i <= n; i++ {
 		fmt.Fscan(buf, &p[i])
+	}
+	cnt := make([][]int, Bits)
+	for b := 0; b < Bits; b++ {
+		cnt[b] = make([]int, n+1)
+		for i := 1; i <= n; i++ {
+			cnt[b][i] = cnt[b][i-1] + (p[i]>>b)&1
+		}
+	}
+	xors := make([]int, n+1)
+	for i := 1; i <= n; i++ {
+		xors[i] = xors[i-1] ^ p[i]
 	}
 	fmt.Fscan(buf, &q)
 	for i := 0; i < q; i++ {
 		var c, l, r int
 		fmt.Fscan(buf, &c, &l, &r)
-		l--
-		r--
 		switch c {
 		case 1:
-			res := p[l]
-			for i := l + 1; i <= r; i++ {
-				res &= p[i]
+			ans := 0
+			for b := 0; b < Bits; b++ {
+				if cnt[b][r]-cnt[b][l-1] == r-l+1 {
+					ans |= 1 << b
+				}
 			}
-			fmt.Println(res)
+			fmt.Println(ans)
 		case 2:
-			res := p[l]
-			for i := l + 1; i <= r; i++ {
-				res |= p[i]
+			ans := 0
+			for b := 0; b < Bits; b++ {
+				if cnt[b][r]-cnt[b][l-1] > 0 {
+					ans |= 1 << b
+				}
 			}
-			fmt.Println(res)
+			fmt.Println(ans)
 		case 3:
-			res := p[l]
-			for i := l + 1; i <= r; i++ {
-				res ^= p[i]
-			}
-			fmt.Println(res)
+			fmt.Println(xors[r] ^ xors[l-1])
 		}
 	}
 }

07e65822e2adf08cb6a27ce6de9b4739113e1fa6
sumire <u333hakuto@gmail.com>
2026-09-02 21:17:45 +0900

    refactor: simplify grid movement and direction state management

--- a_rank_level_up_problems/snake_move/boss/main.go
+++ a_rank_level_up_problems/snake_move/boss/main.go
@@ -10,22 +10,20 @@ func main() {
 	in := bufio.NewReader(os.Stdin)
 	var x, y, n int
 	fmt.Fscan(in, &x, &y, &n)
-	moveLX := []int{-1, 0, 1, 0}
-	moveLY := []int{0, -1, 0, 1}
+	moveX := []int{0, 1, 0, -1}
+	moveY := []int{-1, 0, 1, 0}
 	dir := 0
 	for i := 0; i < n; i++ {
 		var d string
 		fmt.Fscan(in, &d)
 		if d == "L" {
-			x += moveLX[dir]
-			y += moveLY[dir]
-			dir--
+			dir += 3
 		} else {
-			x -= moveLX[dir]
-			y -= moveLY[dir]
-			dir++
+			dir += 1
 		}
+		dir %= 4
+		x += moveX[dir]
+		y += moveY[dir]
 		fmt.Println(x, y)
-		dir = (dir + 4) % 4
 	}
 }
